cac250f8f31ff5a2c0e8b5f644f633e389d22898
kim yong chan <skslrk00@naver.com>
2020-09-24 15:05:38 +0900

    1. 데이터 암호화(db, email)

--- springedu4/src/main/java/com/kh/portfolio/common/util/PropertiesEncryptor.java
+++ springedu4/src/main/java/com/kh/portfolio/common/util/PropertiesEncryptor.java
@@ -7,21 +7,21 @@ public class PropertiesEncryptor {
 	public static void main(String[] args) {
 		StandardPBEStringEncryptor enc = new StandardPBEStringEncryptor();
 		//�븫�샇�궎
-		enc.setPassword("wjdtjdghk");
+		enc.setPassword("");
 		
 		//db�븫�샇�솕
 	//	System.out.println(enc.encrypt("oracle.jdbc.driver.OracleDriver"));
 		System.out.println(enc.encrypt("net.sf.log4jdbc.sql.jdbcapi.DriverSpy"));
 //		System.out.println(enc.encrypt("jdbc:oracle:thin:@127.0.0.1:1521:xe"));
 		System.out.println(enc.encrypt("jdbc:log4jdbc:oracle:thin:@127.0.0.1:1521:xe"));
-		System.out.println(enc.encrypt("mycloset"));
-		System.out.println(enc.encrypt("mycloset"));
+		System.out.println(enc.encrypt(""));
+		System.out.println(enc.encrypt(""));
 		//mail�븫�샇�솕
-		System.out.println(enc.encrypt("1404jsh@gmail.com"));
-		System.out.println(enc.encrypt("wjdtjdghk3801"));
+		System.out.println(enc.encrypt(""));
+		System.out.println(enc.encrypt(""));
 		
 		//蹂듯샇�솕
-		System.out.println(enc.decrypt("eapAp5DOvmae03TNmXUKz0VFsIvCp5Rb"));
+		System.out.println(enc.decrypt(""));
 	}
 
 }
